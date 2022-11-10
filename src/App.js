899b78481354143cd0ff49ef08e766a37f0b3c43
LassiMik <lassi@vartiaiset.com>
2022-11-10 16:20:32 +0200

    asd asd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import Todotable from './components/Todolist';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+      setTodo({...todo, [event.target.name]: event.taget.value});
+  }
+
+  const addTodo = (event) => {
+      event.preventDefault()
+      setTodos([...todos, todo])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+          <form onSubmit={addTodo}>
+              <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+              <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
+              <input type="submit" value="Add"/>
+          </form>
+          <Todotable todos={todos}/>
+      </div>
+  )
 }
 
 export default App;
